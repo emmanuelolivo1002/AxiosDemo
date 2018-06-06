5b452ab32e44fa9793fa4d0b13a0c5fefb7ea84f
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-06-06 11:02:26 -0500

    delete

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -18,6 +18,11 @@ class FullPost extends Component {
       }
     }
   }
+
+  deletePostHandler = () => {
+    axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id);
+  }
+
   render() {
     let post = <p style={{
         textAlign: 'center'
@@ -32,7 +37,7 @@ class FullPost extends Component {
         <h1>{this.state.loadedPost.title}</h1>
         <p>{this.state.loadedPost.body}</p>
         <div className="Edit">
-          <button className="Delete">Delete</button>
+          <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
         </div>
       </div>);
     }
